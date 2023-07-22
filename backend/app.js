5c21790dc885ef97d7f3e3e5178da231f54d72fb
Anna Zakharova <palmiraptz89@yandex.ru>
2023-07-22 17:08:38 +0300

    fix: Fix server port

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const errorHandler = require('./middlewares/error-handler');
 const mainRouter = require('./routes/index');
 
-const { PORT = 3001, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
+const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 const app = express();
 
 mongoose.connect(DB_URL);
